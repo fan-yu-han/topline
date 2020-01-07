8f4aa7a444a80af5ac491211967cb7f6299f902a
hanyufan <1041218477@qq.com>
2020-01-07 17:25:57 +0800

    登录-封装请求接口

---
+++ src/api/user.js
@@ -0,0 +1,22 @@
+// 用户相关的请求模块
+
+import request from '@/utils/request'
+
+export const login = (data) => {
+  return request({
+    // 请求方法
+    methdo: 'POST',
+
+    // 请求路径
+    url: '/app/v1_0/authorizations',
+
+    // 请求头
+    // headers: {},
+
+    // Query 查询参数
+    // parsms: {},
+
+    // Body 请求提参数
+    data: data
+  })
+}
