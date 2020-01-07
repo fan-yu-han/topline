c9a1dedb7c0e902916bf8696fa7635d68fc0e668
hanyufan <1041218477@qq.com>
2020-01-07 15:01:04 +0800

    创建登录组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,12 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/login',
+    name: 'login',
+    // @ 是src 目录的别名 这是 VueCli 项目中特殊的提供
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = new VueRouter({
