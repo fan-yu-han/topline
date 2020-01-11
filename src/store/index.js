41f5eb2fa496bbd1df6828ad122483ce4eb9bb7e
hanyufan <1041218477@qq.com>
2020-01-11 09:30:57 +0800

    token处理-优化本地存储模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { getItem, setItem } from '@/utils/storage'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: JSON.parse(window.localStorage.getItem('user'))
+    user: getItem
+    // user: JSON.parse(window.localStorage.getItem('user'))
   },
   mutations: {
     setUser (state, data) {
       state.user = data
       // 为了防止刷新数据丢失，这 需要把数据放到本地 存储持久化
-      window.localStorage.setItem('user', JSON.stringify(state.user))
+      setItem('user', state.user)
+      // window.localStorage.setItem('user', JSON.stringify(state.user))
     }
   },
   actions: {
