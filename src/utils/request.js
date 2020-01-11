6dfa5d44b6729204d7880351e3e81255d50c667d
hanyufan <1041218477@qq.com>
2020-01-11 10:15:01 +0800

    token处理 - 拦截器 发送token

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,9 @@
  */
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+// 在 JavaScript 模块中直接 import 获取容器即可
+// 这里得到的 store 和你在组件中的 this.$store 是一样一样的
+import store from '@/store'
 
 // axios.create 方法：复制一个 axios
 const request = axios.create({
@@ -24,5 +27,29 @@ request.defaults.transformResponse = [function (data) {
     return {}
   }
 }]
+
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 统一设置token
+  const { user } = store.state
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+// 相应拦截器
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
 
 export default request
