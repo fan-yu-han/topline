65e01132d3eebda6832bb6af9c4153911f8c5280
hanyufan <1041218477@qq.com>
2020-01-13 20:51:19 +0800

    频道编辑-展示剩余频道

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -15,9 +15,9 @@
     <van-cell title="推荐频道" :border="false" />
     <van-grid :gutter="10">
       <van-grid-item
-        v-for="value in 8"
-        :key="value"
-        text="文字"
+        v-for="channel in remainingChannels"
+        :key="channel.id"
+        :text="channel.name"
       />
     </van-grid>
   </div>
@@ -42,7 +42,21 @@ export default {
       allChannels: [] // 所有频道
     }
   },
-  computed: {},
+  computed: {
+    remainingChannels () {
+      const { allChannels, userChannels } = this
+      // 剩余频道 = 所有频道 - 我的频道
+      const channels = []
+      // 遍历所有频道
+      allChannels.forEach(item => {
+        // 如果我的频道中不包含当前被遍历的频道，则要
+        if (!userChannels.find(c => c.id === item.id)) {
+          channels.push(item)
+        }
+      })
+      return channels
+    }
+  },
   watch: {},
   created () {
     this.loadAllChannels()
